f1bfa738836219c91708df5dcf65445dd58a9d2f
arturocueva81 <ajcs81@hotmail.com>
2026-04-16 17:31:21 -0500

    taller 2 parte 1_2. funcion calcularDisponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,8 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIOfunction calcularDisponible(ingresos, egresos) {
+function calcularDisponible(ingresos, egresos) {
+    let disponible = ingresos - egresos;
+    if (disponible < 0) {
+        return 0;
+    }
+    return disponible;
+}

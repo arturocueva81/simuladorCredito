032f4540bd66a9af540347a1d2c6ada84f4cb9d4
arturocueva81 <ajcs81@hotmail.com>
2026-04-16 17:32:50 -0500

    taller 2 parte 1_3. funcion calcular y onclick

--- simulador.js
+++ simulador.js
@@ -1,1 +1,15 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular() {
+    // Leer ingresos y egresos como números decimales
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+
+    // Llamar a la función calcularDisponible
+    let disponible = calcularDisponible(ingresos, egresos);
+
+    // Mostrar resultado en pantalla
+    document.getElementById("spnDisponible").textContent = "USD " + disponible.toFixed(2);
+}
+
+// Evento del botón
+document.getElementById("btnCalcularCredito").onclick = calcular;
